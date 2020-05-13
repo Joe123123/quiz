3d7d6f70a9e750162eb79b4bf8406974e1731215
Zhao Wang <wangz2266@gmail.com>
2020-05-13 16:18:05 -0700

    fetch and show questions

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -2,7 +2,7 @@
   <div class="question-box-container">
     <b-jumbotron>
       <template v-slot:lead>
-        Some questions
+        {{ currentQuestion.question | parse }}
       </template>
 
       <hr class="my-4" />
@@ -12,7 +12,21 @@
       </p>
 
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next</b-button>
+      <b-button variant="success" href="#" @click="next">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+  },
+  filters: {
+    parse: function(value) {
+      return value.replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+    },
+  },
+};
+</script>
